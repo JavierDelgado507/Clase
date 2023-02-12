2bb8fe17604ea811450ba6a93711cc3226955dce
JavierDelgado507 <javierdelgado507@gmail.com>
2023-02-12 21:47:30 +0100

    modified:   TrabajosClase/M9/3-boles/main.js 	new file:   TrabajosClase/M9/Practica1-2 UF2-M9/.vscode/settings.json 	modified:   TrabajosClase/M9/Practica1-2 UF2-M9/index.html 	modified:   TrabajosClase/M9/Practica1-2 UF2-M9/style.css

--- TrabajosClase/M9/3-boles/main.js
+++ TrabajosClase/M9/3-boles/main.js
@@ -14,61 +14,88 @@ function random(min, max) {
 // funció per generar un color aleatori
 function randomRGB() {
   //@TODO3: Retornar un color aleatori rgb (exemple: rgb(valor,valor,valor) o #a1b2c3). Es pot aprofitar la funció random()
-  var color = "("+random(255)+"," + random(255) + "," + random(255) +")";
-	return "rgb" + color;
+  return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
 }
 
 class Bola {
 
-   constructor(x, y, velX, velY, color, tamany) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.tamany = tamany;
-   }
+  constructor(x, y, velX, velY, color, tamany) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.tamany = tamany;
+  }
 
-   dibuixar() {
-     //@TODO 1: Dibuixar una circumferència. Per fer-ho utilitzarem la funció "arc()" a la posició x,y de radi "tamany". L'arc ha de començar a 0 i acabar en (2*Pi) per a que formi un cercle
-   }
+  dibuixar() {
 
-   update() {
-     //@TODO5: A cada execució:
-     //posició x de la bola incrementa tants píxels com la velocitat X
-     //posició y de la bola incrementa tants píxels com la velocitat Y
-     //D'aquesta manera la bola avançara X pixels (segons la velocitat x) i Y pixels (segons la velocitat y)
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.tamany, 0, 2 * Math.PI);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
 
-     //@TODO6: Hem d'evitar que les boles sortin del frame.
-     // Hem de fer-les rebotar quan arribin i toquin la paret del canvas
-     // Per a que rebotin:
-     //   si ho fan a la paret dreta o esquerra, la velocitat x canvia de direcció (Per exemple, si té velocitat X positiva, passa aquesta a negativa. O si és negativa, passa a positiva)
-     //   si ho fan a la paret d'adalt o abaix, la velocitat y canvia de direcció (Per exemple, si té velocitat Y positiva, passa aquesta a negativa.  O si és negativa, passa a positiva)
-     // S'ha de tenir en compte el tamany de la bola. Aquesta ha de rebotar quan la superfície de la bola toqui la paret.
+  update() {
+    //@TODO5: A cada execució:
+    //posició x de la bola incrementa tants píxels com la velocitat X
+    //posició y de la bola incrementa tants píxels com la velocitat Y
+    this.x += this.velX;
+    this.y += this.velY;
 
-   }
+    //D'aquesta manera la bola avançara X pixels (segons la velocitat x) i Y pixels (segons la velocitat y)
 
-   
-   collisio() {
-     //@TODO8:
-     //Mirem la bola actual amb cadascuna de les boles per veure si es toquen. Si es toquen, el color de les 2 boles es canvien per un color aleatori nou.
-     //
-     //la distància entre dos punts és d=√((x_2-x_1)²+(y_2-y_1)²)
-     //Si aquesta distancia és menor que la suma dels tamanys de les boles => llavors es toquen: canviem els colors de manera aleatòria de les 2 boles
+    //@TODO6: Hem d'evitar que les boles sortin del frame.
+    // Hem de fer-les rebotar quan arribin i toquin la paret del canvas
+    // Per a que rebotin:
+    //   si ho fan a la paret dreta o esquerra, la velocitat x canvia de direcció (Per exemple, si té velocitat X positiva, passa aquesta a negativa. O si és negativa, passa a positiva)
+    //   si ho fan a la paret d'adalt o abaix, la velocitat y canvia de direcció (Per exemple, si té velocitat Y positiva, passa aquesta a negativa.  O si és negativa, passa a positiva)
+    // S'ha de tenir en compte el tamany de la bola. Aquesta ha de rebotar quan la superfície de la bola toqui la paret.
+    if (this.x + this.tamany > width || this.x - this.tamany < 0) {
+      this.velX = -this.velX;
+    }
 
-   }
+    if (this.y + this.tamany > height || this.y - this.tamany < 0) {
+      this.velY = -this.velY;
+    }
+  }
 
+
+  collisio() {
+    //@TODO8:
+    //Mirem la bola actual amb cadascuna de les boles per veure si es toquen. Si es toquen, el color de les 2 boles es canvien per un color aleatori nou.
+    //
+    //la distància entre dos punts és d=√((x_2-x_1)²+(y_2-y_1)²)
+    //Si aquesta distancia és menor que la suma dels tamanys de les boles => llavors es toquen: canviem els colors de manera aleatòria de les 2 boles
+    for (let j = 0; j < boles.length; j++) {
+      if (!(this === boles[j])) {
+        const dx = this.x - boles[j].x;
+        const dy = this.y - boles[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.tamany + boles[j].tamany) {
+          this.velX = -(this.velX);
+          this.velY = -(this.velY);
+          boles[j].velX = -(boles[j].velX);
+          boles[j].velY = -(boles[j].velY);
+          this.color = randomRGB();
+          boles[j].color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 //CREACIÓ DE BOLES
 const boles = [];
 
 while (boles.length < numboles) {
-   //tamany de la bola en pixels
-   const tamany = random(10,20);
+  //tamany de la bola en pixels
+  const tamany = random(10, 20);
 
 
-   //@TODO 2:
+  //@TODO 2:
   //Els valors s'han de canviar
   //      x: número aleatori entre (el tamany de la bola) i (l'amplada de la finestra - tamany de la bola). Així forcem que la bola no sobrepassi la finestra al dibuxar-la
   //      y: número aleatori entre el (el tamany de la bola) i (l'alçada de la finestra - tamany de la bola). Així forcem que la bola no sobrepassi la finestra al dibuxar-la
@@ -78,34 +105,37 @@ while (boles.length < numboles) {
   //      tamany: tamany
   //
   //paràmetres de bola: pos x, pos y, velX, velY, color, tamany
-   const bola = new Bola(
-      100,
-      100,
-      0,
-      0,
-      '#ffaaaa',
-      50,
-   );
+  while (boles.length < numboles) {
+    const tamany = random(10, 20);
+    const bola = new Bola(
+      random(tamany, width - tamany),
+      random(tamany, height - tamany),
+      random(-7, 7),
+      random(-7, 7),
+      randomRGB(),
+      tamany
+    );
 
-  boles.push(bola);
+    boles.push(bola);
+  }
 }
 
 function loop() {
-   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-   ctx.fillRect(0, 0,  width, height);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
 
-   for (const bola of boles) {
-     bola.dibuixar();
+  for (const bola of boles) {
+    bola.dibuixar();
 
-     //@TODO4: activar update
-     //bola.update();
+    //@TODO4: activar update
+    bola.update();
 
-     //@TODO7: activar collisio
-     // bola.collisio();
-   }
+    //@TODO7: activar collisio
+    bola.collisio();
+  }
 
-    //funció que executa loop a cada frame d'una animació.
-    requestAnimationFrame(loop);
+  //funció que executa loop a cada frame d'una animació.
+  requestAnimationFrame(loop);
 }
 
 
