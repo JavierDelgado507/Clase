7dbc6e3addeabbff961668e89ff23d62d3a4826c
JavierDelgado507 <javierdelgado507@gmail.com>
2023-03-29 16:49:17 +0200

    Java juego base

---
+++ TrabajosClase/M6/JAVAGAME/js/main.js
@@ -0,0 +1,171 @@
+const canvas = document.getElementById("miCanvas");
+canvas.width = "800";
+canvas.height = "600";
+
+const anchoGeorge = 48;
+const altoGeorge = 48;
+const anchoMoneda = 16;
+const altoMoneda = 16;
+
+let indiceGeneral = 0;
+let indiceGeorge = 0;
+let indiceMonedaDorada = 0;
+let indiceMonedaTransparente = 0;
+
+let gx;
+let gy;
+let mdx;
+let mdy;
+let mty;
+let mtx;
+
+//ctx == context
+const ctx = canvas.getContext("2d");
+
+const fondo = new Image();
+fondo.src = "../assets/hierba.jpg";
+
+const george = new Image();
+george.src = "./../assets/george.png";
+
+const monedaDorada = new Image();
+monedaDorada.src = "./../assets/coin_avr_color.png";
+
+const monedaTransparente = new Image();
+monedaTransparente.src = "./../assets/coin_avr.png";
+
+fondo.onload = function () {
+  //UNA sola Vez
+  posicionarGeorge();
+  posicionarMonedaDorada();
+  posicionarMonedaTransparente();
+
+  //Dibujar se ejecuta en bucle
+  dibujar();
+};
+function posicionarGeorge() {
+  gx = canvas.width / 2 - anchoGeorge / 2; //Coordenada X aleatoria
+  gy = canvas.height / 2 - altoGeorge / 2; //Cpordenada y aleatoria
+}
+
+function posicionarMonedaDorada() {
+  mdx = Math.floor(Math.random() * (canvas.width - anchoMoneda * 2));
+  mdy = Math.floor(Math.random() * (canvas.height - altoMoneda * 2));
+}
+
+function posicionarMonedaTransparente() {
+  mtx = Math.floor(Math.random() * (canvas.width - anchoMoneda * 2));
+  mty = Math.floor(Math.random() * (canvas.height - altoMoneda * 2));
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "s") {
+    if (gy + 5 + altoGeorge < canvas.height) {
+      gy += 5;
+    }
+  } else if (event.key === "w") {
+    if (gy - 5 > 0) {
+      gy -= 5;
+    }
+  } else if (event.key === "a") {
+    if (gx - 5 > 0) {
+      gx -= 5;
+    }
+  } else if (event.key === "d") {
+    if (gx + 5 + anchoGeorge < canvas.width) {
+      gx += 5;
+    }
+  }
+});
+
+function colisionMonedes() {
+  if (
+    gx < mdx + anchoMoneda &&
+    gx + anchoGeorge > mdx &&
+    gy < mdy + altoMoneda &&
+    gy + altoGeorge > mdy
+  ) {
+    posicionarMonedaDorada();
+
+  }
+
+  if (
+    gx < mtx + anchoMoneda &&
+    gx + anchoGeorge > mtx &&
+    gy < mty + altoMoneda &&
+    gy + altoGeorge > mty
+  ) {
+    posicionarMonedaTransparente();
+  }
+}
+
+function dibujar() {
+  //LIMPIAR CANVAS
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //PATRON CON LA IMAGEN DEL FONDO
+  const patron = ctx.createPattern(fondo, "repeat");
+  ctx.fillStyle = patron;
+  //PINTA EL FONDO EN EL CANVAS
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(
+    george, //Imagen George
+    0, //Posicion X en la imagen
+    indiceGeorge * altoGeorge, //Posicion Y en la Imagen
+    anchoGeorge, //Ancho de la Imagen
+    altoGeorge, //Alto de la imagen
+    gx, //Posicion X en la pantalla
+    gy, //Posicion Y en la pantalla
+    anchoGeorge, //Ancho en la pantalla
+    altoGeorge //Alto en la pantalla
+  );
+
+  ctx.drawImage(
+    monedaDorada, //Imagen moneda Dorada
+    indiceMonedaDorada * anchoMoneda, //Posicion X en la imagen
+    0, //Posicion Y en la Imagen
+    anchoMoneda, //Ancho de la Imagen
+    altoMoneda, //Alto de la imagen
+    mdx, //Posicion X en la pantalla
+    mdy, //Posicion Y en la pantalla
+    anchoMoneda * 1.5, //Ancho en la pantalla
+    altoMoneda * 1.5 //Alto en la pantalla
+  );
+
+  ctx.drawImage(
+    monedaTransparente, //Imagen moneda Dorada
+    indiceMonedaDorada * anchoMoneda, //Posicion X en la imagen
+    0, //Posicion Y en la Imagen
+    anchoMoneda, //Ancho de la Imagen
+    altoMoneda, //Alto de la imagen
+    mtx, //Posicion X en la pantalla
+    mty, //Posicion Y en la pantalla
+    anchoMoneda * 1.5, //Ancho en la pantalla
+    altoMoneda * 1.5 //Alto en la pantalla
+  );
+
+  if (indiceGeneral % 10 == 0) {
+    indiceMonedaDorada++;
+    if (indiceMonedaDorada % 2 == 0) {
+      indiceGeorge++;
+    }
+  }
+
+  indiceGeneral++;
+
+  if (indiceGeorge >= 4) {
+    indiceGeorge = 0;
+  }
+
+  if (indiceMonedaDorada >= 8) {
+    indiceMonedaDorada = 0;
+  }
+
+  if (indiceGeneral >= 1000000) {
+    indiceGeneral = 0;
+  }
+  colisionMonedes();
+  // setInterval(dibujar,100);
+
+  requestAnimationFrame(dibujar);
+}
